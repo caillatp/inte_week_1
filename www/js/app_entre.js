14d77ddf6b902bcd2b1bab0806cd4221af77fbd8
caillatp <pierre-alexandre.caillat@hetic.net>
2016-11-17 18:25:46 +0100

    final code js

--- www/js/app_entre.js
+++ www/js/app_entre.js
@@ -1,10 +1,7 @@
-var elem1 = 0;
-var elem2 = 0;
-var elem3 = 0;
-var Count = 0;
-var max = 0;
+var elem1 = 0, elem2 = 0, elem3 = 0, max = 0;
+local_file = "../html/beta_app.html";
 $( init );
-function init() {
+function init() { 
     $('.drag').draggable({
         containment: '.container',
         snap: false,
@@ -20,20 +17,15 @@ function init() {
   // drop unique par choix pour spécifier le nombre de contenu
     $('.drop').droppable( {
         drop: function(event, ui){
-        $( "img" ).remove("#" + ui.draggable.attr('id') );
+        if (max == 0){
+        $( "img" ).remove("#" + ui.draggable.attr('id'));
         handleDropEvent(event, ui);
-        //var $this = $(this);
-        //ui.draggable.position({
-        //my: "center",
-        //at: "center",
-        //of: $this,
-       //using: function(pos) {
-       // $(this).animate(pos, 200, "linear");  
-        }
+        }else{
+            Clearall();
+        }    
+    }
     });
   }
-//});
-//}
 
 
 // Afficher le prévisionnage 
@@ -43,7 +35,7 @@ function handleDropEvent( event, ui ) {
       idx = 6;
       elem1 = 1;
       while (idx > 0){
-      $('.content_'+ idx ).prepend('<img class="rotate" src="../img/app_img/no_pique/plat/'+ draggable.attr('id') +'.png" />');
+      $('.content_'+ idx ).prepend('<img class="rotate" src="../img/app_img/elem/'+ draggable.attr('id') +'.png" />');
         idx--;  
         }
     }else if (elem1 == 1 && elem2 == 0){
@@ -51,7 +43,7 @@ function handleDropEvent( event, ui ) {
         idx = 5;
         while (idx > 1){
             $('.content_' + idx ).empty(); 
-            $('.content_' + idx ).prepend('<img class="rotate135" src="../img/app_img/no_pique/plat/'+ draggable.attr('id') +'.png" />');
+            $('.content_' + idx ).prepend('<img class="rotate135" src="../img/app_img/elem/'+ draggable.attr('id') +'.png" />');
             idx = idx - 3;
         }
     }else if (elem1 == 1 && elem2 == 1){
@@ -59,31 +51,25 @@ function handleDropEvent( event, ui ) {
         idx = 4;
         while (idx > 0){
             $('.content_' + idx ).empty(); 
-            $('.content_' + idx ).prepend('<img class="rotate45" src="../img/app_img/no_pique/plat/'+ draggable.attr('id') +'.png" />');
+            $('.content_' + idx ).prepend('<img class="rotate45" src="../img/app_img/elem/'+ draggable.attr('id') +'.png" />');
             idx = idx - 3;
         }
+    }if (elem1 == 1 && elem2 == 1 && elem3 == 1){
+        max = 1;
     }
 }
 
 function Clearall(){
-   // delete de la prev
     var idx = 6;
     while (idx > 0){
          $('.content_' + idx ).empty(); 
         idx--;
     }
-    //reset de id=menu-app
     elem1 = 0;
     elem2 = 0;
     elem3 = 0;
+    max = 0;
     $( "#menu-app" ).empty(); 
-    $( "#menu-app" ).load( "../html/beta_app.html #menu-content" ); // réintergation des images après supr
+    $( "#menu-app" ).load( local_file +" #menu-content" );
     setTimeout(function(){ $( init ); }, 1000);
 }
-
-/*
-function handleDragStop( event, ui ) {
-  var offsetXPos = parseInt( ui.offset.left );
-  var offsetYPos = parseInt( ui.offset.top );
-  alert( "Drag stopped!\n\nOffset: (" + offsetXPos + ", " + offsetYPos + ")\n");
-}*/
